02360c09d194e88ed03cc478cbfc355c1a669ee3
eucylin <eucylin@gmail.com>
2014-09-22 03:25:51 +0800

    greed

--- ALG/Greedy Gift Givers.cpp
+++ ALG/Greedy Gift Givers.cpp
@@ -8,7 +8,7 @@ LANG: C++11
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <unordered_map>
+#include <map>
 using namespace std;
 
 int main(){
@@ -16,28 +16,28 @@ int main(){
     ifstream fin ("gift1.in");
     int NP;
     fin >> NP;
-    unordered_map<int, pair<string, int>> d;
-    //map<string ,int>::const_iterator it;
+    map<string, int> d;
+    vector<string> name;
     for (int i = 0; i < NP; i++){
-    	string name;
-    	fin >> name;
-    	d[i] = pair(name, 0);
+    	string name__;
+    	fin >> name__;
+    	name.push_back(name__);
     }
     for (int i = 0; i < NP; i++){
     	int j, money;
     	string giver;
     	fin >> giver;
     	fin >> money >> j;
-    	if (money == 0 && j == 0){break;}
+    	if (money == 0 && j == 0){continue;}
     	for (int i = 0; i < j; i++){
     		string receiver;
     		fin >> receiver;
-    		d[j] += money / j;
+    		d[receiver] += money / j;
     	}
-    	d[giver] = d[giver] - money + money % j;
+    	d[giver] -= money / j * j;
     }
-    for (auto it = d.begin(); it != d.end(); it++){
-    	fout << it->first << " " << it->second << endl;
+    for (auto iter = name.begin(); iter != name.end(); iter++){
+    	fout << *iter << " " << d[*iter] << endl;
     }
     return 0;
 }
